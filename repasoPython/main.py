4acc308232bf1fffcef67ee95f8994d948b57a75
unknown <mcordobaortiz82@gmail.com>
2023-03-08 15:55:10 -0500

    Se agregan los operadores

---
+++ repasoPython/main.py
@@ -0,0 +1,72 @@
+#tipos de datos primitivos
+#numeros enteros
+
+numero1: int = 57
+print(numero1)
+
+numero2 = 24
+print(numero2)
+
+#numeros reales
+
+numero3: float = 4.9
+print(numero3)
+
+numero4 = 5.6
+print(numero4)
+#Booleanos
+
+esColombiano: bool = True
+esArgentino = False
+print(esColombiano)
+
+#Caracter y cadena de caracteres
+
+mensaje = "Cadena con una comilla simple ', una comilla doble \" y una diagonal invertida\\"
+print(mensaje)
+
+#operadores
+#aritmeticos
+
+numero5 = 9
+numero6 = 12
+suma = numero5 + numero6
+multiplicacion = numero5 * numero6
+resta = numero5 - numero6
+division = numero5 / numero6
+modulo = numero5 % numero6
+print("la suma es : ", suma)
+print("la multiplicacion es :", multiplicacion)
+print(" la resta es :", resta)
+print("la division es :", division)
+print(" el modulo es :", modulo)
+
+#operador de asignacion
+x = 7
+y = 8
+z = 2
+print(x)
+
+#operadores logicos
+#and(y)
+q = 5
+print(q > 4 and q < 9)
+
+#operador or (o)
+p = 4
+print(p > 5 or p < 10)
+
+# operador not
+print(not(p > 2 and q < 7))
+
+#operadores relacionales
+
+valor1 = 7
+valor2 = 9
+
+print(valor1 == valor2)  #igualdad
+print(valor1 > valor2)  #mayor que
+print(valor1 < valor2) #menor que
+print(valor1 >= valor2) #mayor igual que
+print( valor1 <= valor2)#menor igual que
+print(valor1 != valor2) # no igual 
